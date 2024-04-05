d668b97565f530d6b389f361d12030b3bda0797b
Wiktor Piela <wpiela9@gmail.com>
2024-04-05 23:17:31 +0200

    product app added and product model created

---
+++ ecommerce/product/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, default='', blank=False)
+    description = models.TextField(max_length=1000, default='', blank=False)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    brand = models.CharField(max_length=200, default='', blank=False)
+
+    category_choices = [
+        ('ELECTRONICS', 'Electronics'),
+        ('LAPTOPS', 'Laptops'),
+        ('ARTS', 'Arts'),
+        ('FOOD', 'Food'),
+        ('HOME', 'Home'),
+        ('KITCHEN', 'Kitchen'),
+    ]
+
+    category = models.CharField(max_length=30, choices=category_choices)
+    ratings = models.DecimalField(max_digits=3, decimal_places=2, default=0)
+    stock = models.IntegerField(default=0)
+    user = models.ForeignKey
