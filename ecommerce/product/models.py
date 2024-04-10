2d8f96d891e1bd3810fec7e9ff63677c81c507a2
Wiktor Piela <wpiela9@gmail.com>
2024-04-10 18:18:08 +0200

    product model finished and registered in admin panel

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
@@ -21,4 +22,5 @@ class Product(models.Model):
     category = models.CharField(max_length=30, choices=category_choices)
     ratings = models.DecimalField(max_digits=3, decimal_places=2, default=0)
     stock = models.IntegerField(default=0)
-    user = models.ForeignKey
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
