706848c33d015a8445edf80260ccc87b459454a4
Wiktor Piela <wpiela9@gmail.com>
2024-04-10 18:45:16 +0200

    format_suffix_patterns to display json format in browser

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('product.urls')),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
