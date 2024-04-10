ce9e6f7451db9cd0ccfb8a20b1f8fcf63a3f2281
Wiktor Piela <wpiela9@gmail.com>
2024-04-10 18:37:03 +0200

    get all products view added

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+class Products(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
