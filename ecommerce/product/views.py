e5ca436edd37d240e250b4d4d04786bc43233353
Wiktor Piela <wpiela9@gmail.com>
2024-04-11 07:40:35 +0200

    filters applied in view

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
 from rest_framework import generics
+from  django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
 
 class Products(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category', 'stock']
+        
+
+
 
 class ProductDetails(generics.RetrieveAPIView):
     queryset = Product.objects.all()
