3d52732d7043e61ab1b3c6a23ecdf3e245384fae
Wiktor Piela <wpiela9@gmail.com>
2024-04-16 07:58:02 +0200

    new custom filter applied to list apiview

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -2,16 +2,14 @@ from rest_framework import generics
 from  django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
+from .filters import ProductsFilter
 
 class Products(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['category', 'stock']
+    filterset_class = ProductsFilter
         
-
-
-
 class ProductDetails(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
