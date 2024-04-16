3137840af2bfc0e566f420321c8a5c4d65ff7096
Wiktor Piela <wpiela9@gmail.com>
2024-04-16 08:03:30 +0200

    min max price filter added to custom filter

--- ecommerce/product/filters.py
+++ ecommerce/product/filters.py
@@ -1,11 +1,13 @@
-from django_filters.rest_framework import FilterSet, CharFilter
+from django_filters.rest_framework import FilterSet, CharFilter, NumberFilter
 from .models import Product
 
 class ProductsFilter(FilterSet):
 
     keyword = CharFilter(field_name='name', lookup_expr='icontains')
+    min_price = NumberFilter(field_name='price' or 0, lookup_expr='gte')
+    max_price = NumberFilter(field_name='price' or 10000000, lookup_expr='lte')
 
     class Meta:
         model = Product
-        fileds = ('keyword', 'category', 'brand',)
+        fileds = ('keyword', 'category', 'brand', 'min_price', 'max_price',)
         exclude = ('created_at',)
