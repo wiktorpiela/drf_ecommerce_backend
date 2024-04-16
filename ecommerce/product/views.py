6f5d190403160f063b7141a0f302652f2fe734d0
Wiktor Piela <wpiela9@gmail.com>
2024-04-16 08:18:02 +0200

    paginator applied in list api view

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -3,12 +3,14 @@ from  django_filters.rest_framework import DjangoFilterBackend
 from .models import Product
 from .serializers import ProductSerializer
 from .filters import ProductsFilter
+from .paginators import ProductPaginator
 
 class Products(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_class = ProductsFilter
+    pagination_class = ProductPaginator
         
 class ProductDetails(generics.RetrieveAPIView):
     queryset = Product.objects.all()
