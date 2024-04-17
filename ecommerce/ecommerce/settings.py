856c5edde617c069b489d3138acf828adb55fe02
Wiktor Piela <wpiela9@gmail.com>
2024-04-17 08:06:12 +0200

    custom error handler added and applied in settings

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -125,5 +125,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+
+    'EXCEPTION_HANDLER': 'utils.custom_exception_handler.custom_exception_handler',
 }
