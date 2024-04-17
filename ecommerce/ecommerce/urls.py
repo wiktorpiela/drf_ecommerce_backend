9502ef9be728e259dab25251568135509abe4a91
Wiktor Piela <wpiela9@gmail.com>
2024-04-17 07:36:27 +0200

    applied custom error handlers in main urls

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+# from utils.error_views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +9,6 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+handler404 = 'utils.error_views.handler404'
+handler500 = 'utils.error_views.handler500'
