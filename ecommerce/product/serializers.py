cfd803c5f5044e97a052b67921ed84fc01394747
Wiktor Piela <wpiela9@gmail.com>
2024-04-18 20:47:25 +0200

    create product authenticaded user + images from another model in one

--- ecommerce/product/serializers.py
+++ ecommerce/product/serializers.py
@@ -8,8 +8,17 @@ class ProductImagesSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
-    images = ProductImagesSerializer(many=True, read_only=True)
+    images = ProductImagesSerializer(many=True, read_only=True, required=False)
 
     class Meta:
         model=Product
         fields = ('name', 'description', 'price', 'brand', 'category', 'ratings', 'stock', 'user', 'created_at', 'images',)
+
+    def create(self, validated_data):
+        images_data = self.context.get('request').FILES.getlist('images') 
+        product = Product.objects.create(**validated_data)
+
+        for image_data in images_data:
+            ProductImages.objects.create(product=product, image=image_data)
+
+        return product
