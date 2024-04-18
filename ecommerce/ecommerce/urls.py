12694f4950056316ea23f733316079401b9be209
Wiktor Piela <wpiela9@gmail.com>
2024-04-18 20:45:04 +0200

    get token endpoint added

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
-# from utils.error_views import handler404
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('product.urls')),
+    path('api-token-auth/', views.obtain_auth_token)
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
