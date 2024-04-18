e3609dcdde4ed8b0de59b6a626846d72f6004a77
Wiktor Piela <wpiela9@gmail.com>
2024-04-18 19:47:00 +0200

    product images serializer added and related with product model

--- ecommerce/product/serializers.py
+++ ecommerce/product/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from .models import Product
+from .models import *
+
+class ProductImagesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = '__all__'
 
 class ProductSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField()
+    images = ProductImagesSerializer(many=True, read_only=True)
+
     class Meta:
         model=Product
-        fields = ('name', 'description', 'price', 'brand', 'category', 'ratings', 'stock', 'user', 'created_at',)
+        fields = ('name', 'description', 'price', 'brand', 'category', 'ratings', 'stock', 'user', 'created_at', 'images',)
