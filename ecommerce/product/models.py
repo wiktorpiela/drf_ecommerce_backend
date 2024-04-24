8981baa36f9fa58980b0413f661f835e818fd4e2
Wiktor Piela <wpiela9@gmail.com>
2024-04-24 06:55:18 +0200

    added related name to foreignkey field

--- ecommerce/product/models.py
+++ ecommerce/product/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     category = models.CharField(max_length=30, choices=category_choices)
     ratings = models.DecimalField(max_digits=3, decimal_places=2, default=0)
     stock = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='user_products')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
