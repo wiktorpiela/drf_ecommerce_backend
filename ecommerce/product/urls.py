a80d4e701d67528a2022842e304a3317076619af
Wiktor Piela <wpiela9@gmail.com>
2024-04-29 20:12:48 +0200

    endpoint for create review added

--- ecommerce/product/urls.py
+++ ecommerce/product/urls.py
@@ -6,4 +6,6 @@ app_name = 'product'
 urlpatterns = [
     path('all-products/', views.Products.as_view(), name='allProducts'),
     path('product-details/<int:pk>/', views.ProductDetails.as_view(), name='productDetails'),
+    path('product-review-create/', views.ReviewCreateUpdate.as_view(), name='productReviewCreate'),
+    path('product-review-update/<int:pk>/', views.ReviewCreateUpdate.as_view(), name='productReviewUpdate'),
 ]
