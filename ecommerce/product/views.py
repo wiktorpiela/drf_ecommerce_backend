8df6ba1a32ca380ddb93be1a4d8c1381bd5b46d9
Wiktor Piela <wpiela9@gmail.com>
2024-04-30 14:27:03 +0200

    post - product owner cannot add review for this product, put - update review - TODO: user can update their own reviews only

--- ecommerce/product/views.py
+++ ecommerce/product/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics, status
 from  django_filters.rest_framework import DjangoFilterBackend
-from .models import Product, ProductImages
+from .models import Product, ProductImages, Review
 from .serializers import ProductSerializer, ReviewSerializer
 from .filters import ProductsFilter
 from .paginators import ProductPaginator
@@ -8,7 +8,7 @@ from django.db.models import Avg
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
-from .permissions import IsProductOwnerOrReadOnly
+from .permissions import IsOwnerOrReadOnly
 
 class Products(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -24,7 +24,7 @@ class Products(generics.ListCreateAPIView):
 class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsProductOwnerOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
 
     def perform_update(self, serializer):
         serializer.save()
@@ -35,8 +35,11 @@ class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
         return Response({"message": "Product deleted successfully"})
     
 class ReviewCreateUpdate(APIView):
-    permission_classes = [permissions.IsAuthenticated]
 
+    def get_permissions(self):
+        print(self.request.method)
+        return [IsOwnerOrReadOnly() if self.request.method=='PUT' else permissions.IsAuthenticated()]
+    
     def post(self, request, format=None):
         serializer = ReviewSerializer(data=request.data)
         
@@ -46,6 +49,9 @@ class ReviewCreateUpdate(APIView):
             if product.review.filter(user=self.request.user).exists():
                 return Response({'error': 'Review already posted. Only update or delete actions are allowed.'}, status=status.HTTP_400_BAD_REQUEST)
             
+            elif product.user == self.request.user:
+                return Response({'error': 'You are not allowed to post review to your own product.'}, status=status.HTTP_400_BAD_REQUEST)
+            
             else:
                 serializer.save(user=self.request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -53,5 +59,15 @@ class ReviewCreateUpdate(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def put(self, request):
-        pass
+    def put(self, request, pk):
+        try:
+            review = Review.objects.get(pk=pk)
+            serializer = ReviewSerializer(review, data=request.data)
+
+            if serializer.is_valid():
+                serializer.save(user = self.request.user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Review.DoesNotExist:
+            return Response({'error': 'Review doesnt exist'}, status=status.HTTP_400_BAD_REQUEST)
