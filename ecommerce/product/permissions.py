76511b9208082d5dc1daef2304f848f5fb41f81d
Wiktor Piela <wpiela9@gmail.com>
2024-05-06 08:13:58 +0200

    IsOwnerOrAdmin custom permission added

--- ecommerce/product/permissions.py
+++ ecommerce/product/permissions.py
@@ -5,3 +5,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
+    
+class IsOwnerOrAdmin(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.user == request.user
