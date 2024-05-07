e90971aa2eb11c4bdb62afc81858071ba0b9c6a6
Wiktor Piela <wpiela9@gmail.com>
2024-05-07 07:36:19 +0200

    added forgot password endpoint

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -3,7 +3,8 @@ from .views import *
 
 app_name='accounts'
 urlpatterns = [
-    path('users/', UserList.as_view(), name='usersView')
+    path('users/', UserList.as_view(), name='usersView'),
+    path('forgot-password/', ForgotPassword.as_view(), name='ForgotPassword')
 
 ]
 
