9b9c2fda03510f0e3c05d0f1b8f632e406f6ff6c
Wiktor Piela <wpiela9@gmail.com>
2024-05-07 07:33:57 +0200

    added userprofile and signal to create profile once user is created

--- ecommerce/account/models.py
+++ ecommerce/account/models.py
@@ -1,4 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    reset_password_token = models.CharField(max_length=50, blank=True, default='')
+    reset_password_expire = models.DateTimeField(null=True, blank=True)
 
-
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    user = instance
+    if created:
+        profile = Profile(user=user)
+        profile.save()
