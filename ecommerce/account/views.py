e6cb6c229b071f18101af6c4d6312de2625aa5e9
Wiktor Piela <wpiela9@gmail.com>
2024-05-08 07:57:04 +0200

    in ForgotPassword changed variable link, ResetPassword view added

--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer
@@ -35,7 +35,7 @@ class ForgotPassword(APIView):
         user.profile.save()
 
         host = get_current_host(request)
-        link = f'{host}/api/reset_password/token'
+        link = f'{host}/account/reset-password/{token}/'
         body = f"Your password reset link is: {link}"
 
         email = EmailMessage(
@@ -47,4 +47,28 @@ class ForgotPassword(APIView):
         email.send(fail_silently=False)
 
         return Response({'message': f"Password reset email sent to {data['email']}"})
+    
+class ResetPassword(APIView):
+    
+    def post(self, request, token, format=None):
+        data = request.data
+        try:
+            user = User.objects.get(profile__reset_password_token=token)
+        except User.DoesNotExist:
+            return Response({'message': 'User doesnt exist!'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if user.profile.reset_password_expire.replace(tzinfo=None) < datetime.now():
+            return Response({'message': 'Token is expired'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if data['password'] != data['confirm_password']:
+            return Response({'message': 'Passwords missmatch'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user.set_password(data['password'])
+        user.profile.reset_password_token = ''
+        user.profile.reset_password_expire = None
+
+        user.profile.save()
+        user.save()
+
+        return Response({'message': 'Password has been successfully changed'}, status=status.HTTP_200_OK)
 
